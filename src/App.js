513e1feb7b43a83d8b7fea4cd33add290eb554bb
Karan Joseph <101616233+NeroDg12@users.noreply.github.com>
2023-10-15 14:12:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Alert from './components/Alert';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+// import { Routes, Route } from "react-router-dom";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [mode,setMode] = useState("white");
+  const [alert,setAlert] = useState(null);
 
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  // const removeBodyClass = ()=> {
+  //   document.body.classList.remove("bg-white")
+  //   document.body.classList.remove("bg-black")
+  //   document.body.classList.remove("bg-red")
+  //   document.body.classList.remove("bg-blue")about:blank#blocked
+  //   document.body.classList.remove("bg-green")
+  //   document.body.classList.remove("bg-yellow")
+  // }
+  const toggleMode = (cls) => {
+    // removeBodyClass();       
+    // document.body.classList.add("bg-"+cls)
+    if(mode === "white"){
+      setMode("black")
+      document.body.style.background = "#212529"
+      showAlert("Dark Mode has been enabled","success");
+      document.title = "TextUtility-DarkMode"
+    }
+    else{
+      setMode("white")
+      document.body.style.background = "white"
+      showAlert("Light Mode has been enabled","success");
+      document.title = "TextUtility-LightMode"
+    } 
+  }
+
+  return (
+    <>
+
+      <Navbar title = "TexUtils" mode = {mode} toggleMode = {toggleMode}  />
+
+      <Alert alert = {alert}/>
+      <div className="container w-3/4 pl-60">
+      <TextForm  showAlert = {showAlert} heading = "Enter your text here" mode = {mode}/>
+        {/* <Routes> 
+          <Route path="/" element={<TextForm  showAlert = {showAlert} heading = "Enter your text here" mode = {mode}/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes> */}
+      </div>
+
+      
+    
+    </>
+    
+   
+  )
+}
 export default App;
---
+++ src/components/storage.js
@@ -0,0 +1,1 @@
+{/* <span className={`ml-3 text-sm font-medium text-gray-900 dark:text-${props.mode==="white"?"black":"white"}`}>{props.tBtn}</span> */}
